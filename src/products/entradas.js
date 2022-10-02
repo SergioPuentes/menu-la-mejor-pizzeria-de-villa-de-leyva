239d2b14d5d2846d6693d17ce331dbee5e8b5fb4
Sergio Puentes <se.puentesruiz@gmail.com>
2022-10-02 17:45:10 -0500

    Actualizacion de precios

--- src/products/entradas.js
+++ src/products/entradas.js
@@ -2,27 +2,66 @@ const entradas = [
     {
         name:"Pizza Margarita",
         ingredients:"Queso mozzarella, albahaca fresca, salsa napolitana, oregano, tomate, pimienta negra y aceite de oliva virgen extra",
-        price:"12.500 (Personal) || 20.000(Pequeña) || 33.000(Mediana) || 44.000(Grande)"
+        price:"13.500 (Pequeña) || 26.500 (Mediana) || 38.000(Grande)"
     },
     {
         name:"Pizza Napolitana ",
         ingredients:"Queso mozzarella, salsa napolitana, ajo, aceitunas, perejil y tomate",
-        price:"12.500 (Personal) || 20.000(Pequeña) || 33.000(Mediana) || 44.000(Grande)"
+        price:"13.500 (Pequeña) || 26.500 (Mediana) || 38.000(Grande)"
     },
     {
         name:"Pizza Hawaiana",
-        ingredients:"Queso mozzarella, piña dulce, jamon fresco y salsa napolitana",
-        price:"12.500 (Personal) || 20.000(Pequeña) || 33.000(Mediana) || 44.000(Grande)"
+        ingredients:"Queso mozzarella, piña dulce y jamon fresco",
+        price:"13.000 (Pequeña) || 26.500 (Mediana) || 37.500(Grande)"
     },
     {
         name:"Pizza Pollo y Champiñones",
         ingredients:"Queso mozzarella, salsa napolitana, pollo y champiñones freidos",
-        price:"12.500 (Personal) || 20.000(Pequeña) || 33.000(Mediana) || 44.000(Grande)"
+        price:"14.500 (Pequeña) || 27.500 (Mediana) || 38.500(Grande)"
     },
     {
         name:"Pizza de Carnes",
         ingredients:"Queso mozzarella, salsa napolitana, salami, salchicha y chorizo vela",
-        price:"12.500 (Personal) || 20.000(Pequeña) || 33.000(Mediana) || 44.000(Grande)"
-    }
+        price:"14.500 (Pequeña) || 28.500 (Mediana) || 39.000(Grande)"
+    },
+    {
+        name:"Pizza Mexicana",
+        ingredients:"Queso mozzarella, salsa napolitana, maiz dulce, chorizo y jalapeños",
+        price:"14.500 (Pequeña) || 28.000 (Mediana) || 38.500(Grande)"
+    },
+    {
+        name:"Pizza Campesina",
+        ingredients:"Queso mozzarella, salsa napolitana, maiz, tocineta",
+        price:"14.000 (Pequeña) || 27.500 (Mediana) || 37.500(Grande)"
+    },
+//    {
+//        name:"Pizza de la Casa",
+//        ingredients:"Queso mozzarella, salsa napolitana, carne molida, maiz y platano dulce",
+//        price:"29.500 (Mediana) || 39.500(Grande) (no se vende combinada)"
+//    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 ]
 module.exports = entradas

c87a6503fbe011a5782d173fa736f001dba3be7e
Sergio Puentes <se.puentesruiz@gmail.com>
2022-10-05 05:37:16 -0500

    cambio de precios

--- src/products/entradas.js
+++ src/products/entradas.js
@@ -2,37 +2,37 @@ const entradas = [
     {
         name:"Pizza Margarita",
         ingredients:"Queso mozzarella, albahaca fresca, salsa napolitana, oregano, tomate, pimienta negra y aceite de oliva virgen extra",
-        price:"13.500 (Pequeña) || 26.500 (Mediana) || 38.000(Grande)"
+        price:"15.500 (Pequeña) || 28.000 (Mediana) || 38.500(Grande)"
     },
     {
         name:"Pizza Napolitana ",
         ingredients:"Queso mozzarella, salsa napolitana, ajo, aceitunas, perejil y tomate",
-        price:"13.500 (Pequeña) || 26.500 (Mediana) || 38.000(Grande)"
+        price:"15.500 (Pequeña) || 28.000 (Mediana) || 38.500(Grande)"
     },
     {
         name:"Pizza Hawaiana",
         ingredients:"Queso mozzarella, piña dulce y jamon fresco",
-        price:"13.000 (Pequeña) || 26.500 (Mediana) || 37.500(Grande)"
+        price:"15.000 (Pequeña) || 27.500 (Mediana) || 38.000(Grande)"
     },
     {
         name:"Pizza Pollo y Champiñones",
         ingredients:"Queso mozzarella, salsa napolitana, pollo y champiñones freidos",
-        price:"14.500 (Pequeña) || 27.500 (Mediana) || 38.500(Grande)"
+        price:"16.000 (Pequeña) || 28.500 (Mediana) || 39.000(Grande)"
     },
     {
         name:"Pizza de Carnes",
         ingredients:"Queso mozzarella, salsa napolitana, salami, salchicha y chorizo vela",
-        price:"14.500 (Pequeña) || 28.500 (Mediana) || 39.000(Grande)"
+        price:"16.500 (Pequeña) || 29.000 (Mediana) || 39.500(Grande)"
     },
     {
         name:"Pizza Mexicana",
         ingredients:"Queso mozzarella, salsa napolitana, maiz dulce, chorizo y jalapeños",
-        price:"14.500 (Pequeña) || 28.000 (Mediana) || 38.500(Grande)"
+        price:"15.500 (Pequeña) || 28.000 (Mediana) || 38.500(Grande)"
     },
     {
         name:"Pizza Campesina",
         ingredients:"Queso mozzarella, salsa napolitana, maiz, tocineta",
-        price:"14.000 (Pequeña) || 27.500 (Mediana) || 37.500(Grande)"
+        price:"16.000 (Pequeña) || 28.500 (Mediana) || 39.000(Grande)"
     },
 //    {
 //        name:"Pizza de la Casa",

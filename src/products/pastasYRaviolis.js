47b5de6d7b327c3751ccc9748f7a3010289e250b
Sergio Puentes <Se.puentesruiz@gmail.com>
2022-11-11 15:47:36 -0500

    añadiendo perros calientes

--- src/products/pastasYRaviolis.js
+++ src/products/pastasYRaviolis.js
@@ -1,43 +1,18 @@
 const pastasYRaviolis = [
     {
-        name:"En salsa blanca acompañada de pollo grille o tocineta",
-        ingredients:"",
-        price:38000
+        name:"Perro caliente Clasico",
+        ingredients:"Salchicha, queso, papas chip, cebolla caramelizada",
+        price:9500
     },
     {
-        name:"Con Pollo y Tocineta",
-        ingredients:"",
-        price:40000
+        name:"Perro caliente Mexicano",
+        ingredients:"Salchicha, queso, papas chip, cebolla caramelizada, jalapeños, pimenton",
+        price:12000
     },
     {
-        name:"Con Carnes Italianas",
-        ingredients:"",
+        name:"Perro caliente Ranchero",
+        ingredients:"Salchicha, queso mozarrella, papas chip, tocineta, maiz dulce",
         price:45000
-    },
-    {
-        name:"Con Frutos del Mar",
-        ingredients:"",
-        price:59000
-    },
-    {
-        name:"Tradicional",
-        ingredients:"(sin acompañamiento)",
-        price:33000
-    },
-    {
-        name:"Con Vegetales",
-        ingredients:"(opcion vegetariana)",
-        price:36000
-    },
-    {
-        name:"Con Lomo en salsa de la Casa",
-        ingredients:"",
-        price:63000
-    },
-    {
-        name:"Raviolis de Ricotta y Espinaca",
-        ingredients:"Pasta rellena de ricota y espinaca bañados en salsa de la casa o en salsa napolitana",
-        price:31000
-    },
+    }
 ]
 module.exports = pastasYRaviolis
